35a5e93665ccaf31ef0391a7859686b348a9d9e4
Mario Puebla Junior <mariopuebla17@gmail.com>
2024-05-08 10:22:40 -0300

    novas aulas feitas

--- exemplo-explorando/Models/Curso.cs
+++ exemplo-explorando/Models/Curso.cs
@@ -34,7 +34,8 @@ namespace exemplo_explorando.Models
 
             for (int count = 0; count < Alunos.Count; count++)
             {
-                string texto = "Nº " + count + " - " + Alunos[count].NomeCompleto;
+                // string texto = "Nº " + count + " - " + Alunos[count].NomeCompleto;
+                string texto = $"Nº {count + 1} - {Alunos[count].NomeCompleto}";
                 Console.WriteLine(texto);
             }
         }
--- exemplo-explorando/Program.cs
+++ exemplo-explorando/Program.cs
@@ -1,4 +1,38 @@
 ﻿using exemplo_explorando.Models;
+using System.Globalization;
+
+DateTime data = DateTime.Now;
+
+Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
+
+
+
+
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+
+// decimal valorMonetario = 82.40M;
+
+// Console.WriteLine($"{valorMonetario:C}");
+
+// Console.WriteLine(valorMonetario.ToString("C",CultureInfo.CreateSpecificCulture("pt-BR")));
+
+// double porcentagem = .3421;
+
+// Console.WriteLine(porcentagem.ToString("P"));
+
+
+
+
+// string numero1 = "10";
+// string numero2 = "20";
+// string resultado = numero1 + numero2;
+
+// Console.WriteLine(resultado);
+
+
+
+
 
 // Pessoa p1 = new Pessoa();
 // p1.Nome = "Mario";
@@ -6,24 +40,24 @@
 // p1.Idade = 30;
 // p1.Apresentar();
 
-Pessoa p1 = new Pessoa
-    (
-        nome: "Mario", 
-        sobrenome: "Puebla"
-    );
+// Pessoa p1 = new Pessoa
+//     (
+//         nome: "Mario", 
+//         sobrenome: "Puebla"
+//     );
 
-Pessoa p2 = new Pessoa
-    (
-        nome: "Antônio", 
-        sobrenome: "Oliveira"
-    );
+// Pessoa p2 = new Pessoa
+//     (
+//         nome: "Antônio", 
+//         sobrenome: "Oliveira"
+//     );
 
-Curso cursoDeIngles = new()
-    {
-        Nome = "Inglês",
-        Alunos = []
-    };
+// Curso cursoDeIngles = new()
+//     {
+//         Nome = "Inglês",
+//         Alunos = []
+//     };
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
